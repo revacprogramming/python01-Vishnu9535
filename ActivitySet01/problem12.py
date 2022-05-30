b44b28a4b6b49314cf548da717d50dc4c427cbab
vishnu kumar <97020373+Vishnu9535@users.noreply.github.com>
2022-05-30 11:12:46 +0000

    p12

--- ActivitySet01/problem12.py
+++ ActivitySet01/problem12.py
@@ -1,2 +1,12 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+import re
+hand = open('ref2.txt')
+
+m=hand.read()
+x = re.findall('([0-9]+)', m)
+print(x)
+z=0
+for i in x:
+       z=z+int(i)
+print(z)
