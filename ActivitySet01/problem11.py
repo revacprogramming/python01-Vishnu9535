f0017131a7a8d57e0d73f33b1685ecbc03dbbe24
vishnu kumar <97020373+Vishnu9535@users.noreply.github.com>
2022-05-30 11:11:45 +0000

    p11

--- ActivitySet01/problem11.py
+++ ActivitySet01/problem11.py
@@ -1,3 +1,22 @@
-# Tuples
-
-filename = "dataset/mbox-short.txt"
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+counts=dict()
+man=list()
+for line in fhand :
+    if line.startswith('From:'):
+        man=line.split()
+        if man[1] not in counts:
+            counts[man[1]]=1
+        else:
+            counts[man[1]]+=1
+words=counts.values()
+zam=max(words)
+for key in counts:
+    if counts[key]==zam:
+        print(key,zam)
+        
+        
