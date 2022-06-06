c6ef693bb8e9506b289e8534fa86256511ec611f
vishnu kumar <97020373+Vishnu9535@users.noreply.github.com>
2022-06-06 07:50:40 +0000

    actset2 prog1

--- ActivitySet02/problem01.py
+++ ActivitySet02/problem01.py
@@ -1,11 +1,16 @@
 def add(a, b):
   c=a+b
+  print(c)
   return c
 
 
 def main():
-  a = input()  # ...
-  b = input() # ...
+  a = float(input('Enter the number')) 
+  b = float(input('Enter the number')) 
 
-  c = add(a, b)
+  c = add(a,b)
+  
   print(c)
+if __name__ == '__main__':
+    main()
+
