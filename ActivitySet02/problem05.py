7869bb502e445c34769b9cae6d8b2f557880fa60
Vishnu9535 <vk9255888@gmail.com>
2022-06-29 10:32:35 +0530

    done

--- ActivitySet02/problem05.py
+++ ActivitySet02/problem05.py
@@ -2,11 +2,20 @@
 
 def get_cs():
     """get string input"""
-
+    cs=input('ENTER THE STRING')
+    return cs
 
 def cs_to_dict(cs):
     """convert connect string to a dictionary"""
-
+    iot=dict()
+    k=list()
+    d=cs.split(';')
+    for i in d:
+        x=i.split('=')
+        k.append(x)
+    for i in k:
+        iot[i[0]]=i[1]
+    return iot
 
 def dict_to_cs(d):
     """convert a dictionary to connect string"""
