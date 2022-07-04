4e04df0ddd4628154d6136e4c29c84559a6040b2
Vishnu9535 <vk9255888@gmail.com>
2022-07-04 14:46:19 +0530

    done

--- ActivitySet02/problem06.py
+++ ActivitySet02/problem06.py
@@ -1,11 +1,13 @@
-
-
 class Menu:
     """fill in class definition here"""
+    def add(x,y):
+        
+        
+        
 
 
 m = Menu()  # Menu is a class
 m.add("idly", 10)
-m.add("vada". 20)
+m.add("vada", 20)
 
 m.show()
