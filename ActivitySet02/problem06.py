a940182390a8d685db5b34f4c7b408e9d522730f
Vishnu9535 <vk9255888@gmail.com>
2022-07-06 10:31:32 +0530

    done

--- ActivitySet02/problem06.py
+++ ActivitySet02/problem06.py
@@ -1,15 +1,24 @@
 class Menu:
     """fill in class definition here"""
-        
     def add(self,value,number):
+        x=dict()
+        y=dict()
+        self.y=y
         self.value=value
         self.number=number
+        self.x=x
+        self.x={self.value:self.number}
+
+    def __add__(self,other):
+       
+        self.y=Menu({**self.x,**other.x})
+
     def show(self):
-        print(self.value,self.number)
+        for i in self.y:
+            print(i,self.y[i])
+        
 
 m = Menu()     # Menu is a class
 m.add("idly", 10)
-m.show()
 m.add("vada", 20)
 m.show()      
-   
