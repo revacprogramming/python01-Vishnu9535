01a75cbc2d0b116502c010c281fc49fb0175fb96
Vishnu9535 <vk9255888@gmail.com>
2022-07-09 22:44:32 +0530

    done

--- ActivitySet02/problem06.py
+++ ActivitySet02/problem06.py
@@ -1,25 +1,15 @@
+import pdb
+pdb.set_trace()
 class Menu:
     """fill in class definition here"""
     def __init__(self):
-        self=self
-    def add(self,value,number):
-        x=dict()
-        y=dict()
+        self.d=dict()
         
-        self.y=y
-        self.value=value
-        self.number=number
-        self.x=x
-        self.x={self.value:self.number}   
-
-    def __add__(self,other):
-       
-        self.y=Menu({**self.x,**other.x})
-        print(self.y)  
-
+    def add(self,value,number):
+        self.d[value] =number          
     def show(self):
-        for i in self.y:
-            print(i,self.y[i])
+        for i in self.d:
+            print(i,self.d[i])
         
 
 m = Menu()     # Menu is a class
