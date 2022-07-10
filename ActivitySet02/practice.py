7830eb0490fd14039f3beb3a687b15f6ee0e9f16
Vishnu9535 <vk9255888@gmail.com>
2022-07-10 18:27:42 +0530

    done

---
+++ ActivitySet02/practice.py
@@ -0,0 +1,33 @@
+#learning classes and objects
+#class players:
+#     cricket="have fun"
+#     def __init__(self,name,age) -> None:
+#         self.name=name
+#         self.age=age  
+#     def info(self):
+#         print(self.name ,"is", self.age,"years old and" ,self.cricket)
+#     def style(self,position):
+#         return f"{self.name} is {position}"
+#     def sty(self,position):
+#         return f"{self.name} at {position}"
+#     def __str__(self):
+#         return("you are a legend")
+# class classic(players):
+#     def style(self,position="toporder"):
+#         return super().sty(position)       
+# class powerhitter(players):
+#     def style(self,position="finishing"):
+#         return super().sty(position)
+        
+
+# goat=classic('virat',33)
+# # goat.info()
+# # print(goat.style("toporder batsmen"))
+# print(goat.style())
+# print(goat)
+# captain=powerhitter('dhoni',40)
+# # captain.info()
+# print(captain.style())
+# print(captain)
+# print(type(goat))
+# print(type(captain))
--- ActivitySet02/problem07.py
+++ ActivitySet02/problem07.py
@@ -9,38 +9,6 @@ m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special method
     
 print(m)  # should print the menu properly
 
-#learning classes and objects
-#class players:
-#     cricket="have fun"
-#     def __init__(self,name,age) -> None:
-#         self.name=name
-#         self.age=age  
-#     def info(self):
-#         print(self.name ,"is", self.age,"years old and" ,self.cricket)
-#     def style(self,position):
-#         return f"{self.name} is {position}"
-#     def sty(self,position):
-#         return f"{self.name} at {position}"
-#     def __str__(self):
-#         return("you are a legend")
-# class classic(players):
-#     def style(self,position="toporder"):
-#         return super().sty(position)       
-# class powerhitter(players):
-#     def style(self,position="finishing"):
-#         return super().sty(position)
-        
 
-# goat=classic('virat',33)
-# # goat.info()
-# # print(goat.style("toporder batsmen"))
-# print(goat.style())
-# print(goat)
-# captain=powerhitter('dhoni',40)
-# # captain.info()
-# print(captain.style())
-# print(captain)
-# print(type(goat))
-# print(type(captain))
 
 
