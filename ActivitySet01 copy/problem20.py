c09a05d6d3cbbadaedcf0021400d7c9a28006e76
Vishnu9535 <vk9255888@gmail.com>
2022-07-24 08:27:32 +0530

    ..

--- ActivitySet01 copy/problem20.py
+++ ActivitySet01 copy/problem20.py
@@ -7,7 +7,7 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
 import re
-
+  
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
